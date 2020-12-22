f24ac75fbb1999a8cc02ee934bf791a290e907c3
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-12-22 17:44:54 +0530

    Header Bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import Button from '@material-ui/core/Button';
-
+import Header from './ui/Header';
 
 function App() {
   return (
     <div className="App">
+    <Header/>
     <Button variant="contained">Click Me!</Button>
+    
     </div>
   );
 }
-
+ 
 export default App;
