f4a3ca6e027a4ec82f4862837aafc3501b8e1362
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-12-23 13:04:35 +0530

    [MODULE 1 COMPLEED] react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {  ThemeProvider } from '@material-ui/core/styles';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Header from './ui/Header';
 import theme from './ui/Theme';
@@ -7,10 +8,20 @@ import theme from './ui/Theme';
 function App() {
   return (
     <ThemeProvider theme={theme}>  
-
+      <BrowserRouter>
        <Header/>
-    <Button variant="contained">Click Me!</Button>
-    </ThemeProvider>
+         <Switch>
+         <Route exact path="/" component={()=><div>Home</div>} />
+         <Route exact path="/services" component={()=><div>Services</div>} />
+         <Route exact path="/customsoftware" component={()=><div>Custom Software</div>} />
+         <Route exact path="/websites" component={()=><div>Websites</div>} />
+         <Route exact path="/revolution" component={()=><div>Revolution</div>} />
+         <Route exact path="/about" component={()=><div>About</div>} />
+         <Route exact path="/contact" component={()=><div>Contact</div>} />
+         <Route exact path="/estimate" component={()=><div>Estimate</div>} />
+         </Switch>
+       </BrowserRouter>
+       </ThemeProvider>
 
   );
 }
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,80 +1,137 @@
-import React,{useState} from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import { makeStyles } from '@material-ui/core/styles';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Button from '@material-ui/core/Button';
+import React, { useState, useEffect } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
-import logo from '../../assets/logo.svg';
- 
+import logo from "../../assets/logo.svg";
 
 function ElevationScroll(props) {
-    const { children } = props;
-    const trigger = useScrollTrigger({
-      disableHysteresis: true,
-      threshold: 0,
-    });
-  
-    return React.cloneElement(children, {
-      elevation: trigger ? 4 : 0,
-    });
-  }
-  
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
 
-const useStyles =makeStyles(theme=>({
-  toolbarMargin:{
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
     ...theme.mixins.toolbar,
-  // marginBottom:"3em"  upper toolbar se kitne space chodna
+    marginBottom: "1.5em", //upper toolbar se kitne space chodna
   },
-  logo:{
-    height:"5.4em"
+  logo: {
+    marginLeft :"7rem",
+    height: "5.4rem",
   },
-  tabContainer:{
-    marginLeft:'auto'  //sabko right size bhej dega
+  logoContainer:{
+    padding:0,
+    "&:hover":{
+      backgroundColor :"transparent"
+    }
   },
-  tab:{
+  tabContainer: {
+    marginLeft: "auto", //sabko right size bhej dega
+  },
+  tab: {
     ...theme.typography.tab,
-    minWidth:10, //tab ke bitch distance kum kardeta 
-     marginLeft:"25px"//distance in pixel bec. we want different in different devices
+    minWidth: 10, //tab ke bitch distance kum kardeta
+    marginLeft: "25px", //distance in pixel bec. we want different in different devices
   },
-  button:{
+  button: {
     ...theme.typography.estimate,
-    borderRadius:"50px", //4ro tarf de cure kara,
-    marginLeft:"50px",
-    marginRight:"25px",
-    height:"45px"
-  }
-}))
+    borderRadius: "50px", //4ro tarf de cure kara,
+    marginLeft: "50px",
+    marginRight: "25px",
+    height: "45px",
+  },
+}));
 
-export default function Header(props){
-  const classes = useStyles()
-  const [value,setValue] =useState(0)
-  const handleChange = (e,value)=>{
+export default function Header(props) {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+  const handleChange = (e, value) => {
     setValue(value);
-  }
-  return(
+  };
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === "/services" && value !== 1)
+      setValue(1);
+    else if (window.location.pathname === "/revolution" && value !== 2)
+      setValue(2);
+    else if (window.location.pathname === "/about" && value !== 3) setValue(3);
+    else if (window.location.pathname === "/contact" && value !== 4)
+      setValue(4);
+    else if (window.location.pathname === "/estimate" && value !== 5)
+      setValue(5);
+  },[value]);
+  return (
     <React.Fragment>
       <ElevationScroll>
-      <AppBar position="fixed">
-          <Toolbar disableGutters={true} className={classes.logo}><img alt = "company logo" src={logo}/>
-          <Tabs value={value}  onChange={handleChange} className={classes.tabContainer} indicateColor="primary" > {/*indicateColor bottom tab color remove kar rha*/}
-            <Tab className={classes.tab} label="Home" />
-            <Tab className={classes.tab}  label="Services" />
-            <Tab className={classes.tab}  label="The Revolution" />
-            <Tab className={classes.tab}  label="About Us" />
-            <Tab className={classes.tab}  label="Contact Us" />
-          <Button variant="contained" color="secondary" className={classes.button}>Free Estimate</Button>
-          </Tabs>
-  
+        <AppBar position="fixed">
+          <Toolbar disableGutters={true} className={classes.logo}>
+          <Button disableRipple component={Link} to="/" onClick={()=>setValue(0)}className={classes.logoContainer}>  {/*disableRipple logo mein click ke waqt color change nahi hone deta*/}
+            <img alt="company logo" src={logo} />
+            </Button>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+              indicateColor="primary"
+            >
+              {" "}
+              {/*indicateColor bottom tab color remove kar rha*/}
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/"
+                label="Home"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/services"
+                label="Services"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/revolution"
+                label="The Revolution"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/about"
+                label="About Us"
+              /> 
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/contact"
+                label="Contact Us"
+              />
+              <Button
+                variant="contained"
+                color="secondary"
+                className={classes.button}
+              >
+                Free Estimate
+              </Button>
+            </Tabs>
           </Toolbar>
-      </AppBar>
+        </AppBar>
       </ElevationScroll>
       {/*vo button tag hide ho gye tha use show kar rha ,push kar rha jo content chup gya tha*/}
-      <div className={classes.toolbarMargin}/>
-      </React.Fragment>
-)
-
-
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>
+  );
 }
