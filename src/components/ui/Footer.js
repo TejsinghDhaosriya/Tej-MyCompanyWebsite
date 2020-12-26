f9ed48458cdfb4861a9807e5dc6d943f3b848da3
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-12-26 17:07:19 +0530

    [Adding Grid into footer]

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,24 +1,34 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
 import footerAdornment from "../../assets/Footer Adornment.svg";
 const useStyles = makeStyles((theme) => ({
   footer: {
     backgroundColor: theme.palette.common.blue,
     width: "100%",
-    zIndex:1302, //footer ko upar rkahna side bar se
-    position:"relative" //saving for zindex towork
+    zIndex: 1302, //footer ko upar rkahna side bar se
+    position: "relative", //saving for zindex towork
   },
-  adronment:{
-      width:"25em",
-      verticalAlignment:"bottom",
-      [theme.breakpoints.down("md")]:{
-          width:"21em"
-      },
-      [theme.breakpoints.down("xs")]:{
-          width:"15em"
-      }
-  }
+  adronment: {
+    width: "25em",
+    verticalAlignment: "bottom",
+    [theme.breakpoints.down("md")]: {
+      width: "21em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "15em",
+    },
+  },
+  mainContainer: {
+    position: "absolute", //text ko footer ke upar dekhane ke liye,
+  },
+  link: {
+    color: "white",
+    fontFamily: "Arail",
+    fontSize: "0.75rem",
+    fontWeight: "bold",
+  },
 }));
 
 export default function Footer() {
@@ -26,7 +36,73 @@ export default function Footer() {
 
   return (
     <footer className={classes.footer}>
-      <img alt="black decorative slash" src={footerAdornment} className={classes.adronment}/>
+      <Grid container justify="center" className={classes.mainContainer}>
+        <Grid item>
+          <Grid container direction="column">
+            <Grid item className={classes.link}>
+              Home
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container direction="column">
+            <Grid item className={classes.link}>
+              Services
+            </Grid>
+            <Grid item className={classes.link}>
+              Custom Software Development
+            </Grid>
+            <Grid item className={classes.link}>
+              Mobile App Development
+            </Grid>
+            <Grid item className={classes.link}>
+              Website Development
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid container direction="column">
+            <Grid item className={classes.link}>
+              The Revolution
+            </Grid>
+            <Grid item className={classes.link}>
+              Vision
+            </Grid>
+            <Grid item className={classes.link}>
+              Technology
+            </Grid>
+            <Grid item className={classes.link}>
+              Process
+            </Grid>
+          </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container direction="column">
+              <Grid item className={classes.link}>
+                About Us
+              </Grid>
+              <Grid item className={classes.link}>
+                History
+              </Grid>
+              <Grid item className={classes.link}>
+                Team
+              </Grid>
+            </Grid>
+          </Grid>
+        
+        <Grid item>
+          <Grid container direction="column">
+            <Grid item className={classes.link}>
+              Contact Us
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <img
+        alt="black decorative slash"
+        src={footerAdornment}
+        className={classes.adronment}
+      />
     </footer>
   );
 }
