d5a1aa07daaf5565d3d3d28f942ec739f02f937b
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-12-28 18:37:52 +0530

    Revolution Card

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,12 +5,15 @@ import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 import ButtonArrow from "./ui/ButtonArrow";
 import animationData from "../animations/landinganimation/data";
 import customSoftwareIcon from "../assets/Custom Software Icon.svg";
 import mobileAppsIcon from "../assets/mobileIcon.svg";
 import websitesIcon from "../assets/websiteIcon.svg";
+import revolutionBackground from "../assets/repeatingBackground.svg";
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -82,6 +85,21 @@ const useStyles = makeStyles((theme) => ({
       padding: 25,
     },
   },
+  revolutionBackground:{
+    backgroundImage:`url(${revolutionBackground})`,
+    backgroundPosition:"center",
+    backgroundSize:"cover",
+    backgroundRepeat:"no-repeat",
+    height:"100%",
+    width:"100%"
+
+  },
+  revolutionCard:{
+    position:"absolute",
+    boxShadow:theme.shadows[10],
+    borderRadius:15,
+    padding:"10em"
+  }
 }));
 
 export default function LandingPage() {
@@ -260,8 +278,33 @@ export default function LandingPage() {
           </Grid>
         </Grid>
       </Grid>
-      
-      
+      <Grid container style={{height:"100em"}} alignItems="center" justify="center">
+      <Card className={classes.revolutionCard}>
+        <CardContent>
+          <Grid container direction="column" style={{textAlign:"center"}}>
+            <Grid item>
+              <Typography variant="h3">
+                 The Revolution
+              </Typography>
+            </Grid>
+            <Grid item > 
+              <Typography variant="subtitle1">
+                 Visionary insights coupled with cutting-edge technology is a recipe for revolution.
+              </Typography>
+              <Button variant="outlined" className={classes.learnButtonHero}>
+                  <span style={{ marginRight: 10 }}>Learn More</span>
+                  <ButtonArrow
+                    width={15}
+                    height={15}
+                    fill={theme.palette.common.blue}
+                  />
+                </Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+      <div className={classes.revolutionBackground}/>
+      </Grid>
     </Grid>
   );
 }
