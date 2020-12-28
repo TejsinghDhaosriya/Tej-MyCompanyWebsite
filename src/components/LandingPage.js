a890f7ecc5a639e08ec9b31a86574e8aefe10fbb
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-12-28 17:59:47 +0530

    Services-iOS/Android App Development

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,13 +4,12 @@ import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 import ButtonArrow from "./ui/ButtonArrow";
 import animationData from "../animations/landinganimation/data";
 import customSoftwareIcon from "../assets/Custom Software Icon.svg";
 import mobileAppsIcon from "../assets/mobileIcon.svg";
-
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -40,15 +39,15 @@ const useStyles = makeStyles((theme) => ({
     height: 45,
     width: 145,
   },
-  learnButton:{
-      ...theme.typography.learnButton,
-      fontSize:"0.7rem",
-       height:35,
-       padding:5,
-       [theme.breakpoints.down("sm")]:{
-         marginBottom:"2em"
-       }
+  learnButton: {
+    ...theme.typography.learnButton,
+    fontSize: "0.7rem",
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down("sm")]: {
+      marginBottom: "2em",
     },
+  },
   mainContainer: {
     marginTop: "5em",
     [theme.breakpoints.down("md")]: {
@@ -69,21 +68,19 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: "Pacifico",
     color: theme.palette.common.orange,
   },
-  subtitle:{
-      marginBottom:"1em"
+  subtitle: {
+    marginBottom: "1em",
   },
-  icon:{
-      marginLeft:"2em",
-      [theme.breakpoints.down("xs")]:
-      {marginLeft:0}
+  icon: {
+    marginLeft: "2em",
+    [theme.breakpoints.down("xs")]: { marginLeft: 0 },
   },
-  serviceContainer:{
-      marginTop:"12em",
-      [theme.breakpoints.down("sm")]:
-      {
-        padding:25
-      }
-  }
+  serviceContainer: {
+    marginTop: "12em",
+    [theme.breakpoints.down("sm")]: {
+      padding: 25,
+    },
+  },
 }));
 
 export default function LandingPage() {
@@ -141,9 +138,20 @@ export default function LandingPage() {
         </Grid>
       </Grid>
       <Grid item>
-        {/* Services Block */}
-        <Grid container direction="row" className={classes.serviceContainer} justify={matchesSM?"center":undefined}>
-          <Grid item style={{marginLeft:matchesSM?0:"5em",textAlign:matchesSM?"center":undefined}}>
+        {/* Custom Software  Block */}
+        <Grid
+          container
+          direction="row"
+          className={classes.serviceContainer}
+          justify={matchesSM ? "center" : undefined}
+        >
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : "5em",
+              textAlign: matchesSM ? "center" : undefined,
+            }}
+          >
             <Typography variant="h4">Custom Software Developement</Typography>
             <Typography variant="subtitle1" className={classes.subtitle}>
               Save Energy. Save Time. Save Money.
@@ -162,7 +170,52 @@ export default function LandingPage() {
             </Button>
           </Grid>
           <Grid item>
-            <img alt="custom software icon" className={classes.icon}src={customSoftwareIcon} />
+            <img
+              alt="custom software icon"
+              className={classes.icon}
+              src={customSoftwareIcon}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item >
+        {/* iOS/Android Block */}
+        <Grid
+          container
+          direction="row"
+          className={classes.serviceContainer}
+          justify={matchesSM ? "center" :"flex-end"}
+        >
+          <Grid
+            item
+            style={{
+             
+              textAlign: matchesSM ? "center" : undefined,
+            }}
+          >
+            <Typography variant="h4">iOS/Android App Developement</Typography>
+            <Typography variant="subtitle1" className={classes.subtitle}>
+              Extend Functionality. Extend Access. Increase Engagement.
+            </Typography>
+            <Typography variant="subtitle1">
+              Integrate your web experience or create a standalone app {matchesSM ? null: <br/>} with either mobile platform
+        
+            </Typography>
+            <Button variant="outlined" className={classes.learnButton}>
+              <span style={{ marginRight: 10 }}>Learn More</span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
+            </Button>
+          </Grid>
+          <Grid item style={{ marginRight: matchesSM ? 0 : "5em",}}>
+            <img
+              alt="mobile app icon"
+              className={classes.icon}
+              src={mobileAppsIcon}
+            />
           </Grid>
         </Grid>
       </Grid>
